b99ba7de5a4d7bc2bb3ee9f617fd44ddccceb342
Destiny Barrett <DesignXDestiny@gmail.com>
2021-03-17 12:06:24 -0600

    Update and rename experimental.js to Codetech/experimental.js

---
+++ Codetech/experimental.js
@@ -0,0 +1,5 @@
+    function msg(){  
+     alert("Meow, Meow, Meow");  
+    }  
+    
+       document.write("<b>Hello World!</b><br>This text is rendered using javascript.  We're all cats here.");
--- experimental.js
+++
@@ -1,7 +0,0 @@
-    function msg(){  
-     alert("Meow, Meow, Meow");  
-    }  
-     
-
-    
-       document.write("<b>Hello World!</b><br>This text is rendered using javascript.");
